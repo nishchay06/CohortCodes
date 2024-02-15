a367070a7cf61298c00627953e5ede457565bbae
Nishchay <nishchay.mago1290@gmail.com>
2024-02-15 11:45:43 +0530

    recoil optimized

--- week7/week-7-1/src/App.jsx
+++ week7/week-7-1/src/App.jsx
@@ -26,11 +26,11 @@ function CountRenderer() {
 }
 
 function Buttons() {
-  const [count,setCount] = useRecoilState(countAtom)
+  const setCount = useSetRecoilState(countAtom)
   return (
     <>
-      <button onClick={() => setCount(count - 1)}>Decrease</button>
-      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count => count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count => count + 1)}>Increase</button>
     </>
   )
 }
